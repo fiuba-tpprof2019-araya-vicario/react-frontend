26f379a3a7bedf1a57e6a46eaf58274607cdeaee
Walter Araya <naraya@fi.uba.ar>
2020-06-07 00:37:59 -0300

    Move accordion to new folder

--- src/utils/Accordion/Accordion.jsx
+++ src/utils/Accordion/Accordion.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Panel, ListGroup, ListGroupItem } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
-import FullRow from './styles/FullRow';
+import FullRow from '../styles/FullRow';
 import './Accordion.css';
 
 export default class Accordion extends React.Component {
